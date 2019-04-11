97ed2214aa0b75bdcb28319e35ea8780b40a2ac2
BIB007$ <bib7@ool-ad020186.dyn.optonline.net>
2019-04-11 16:50:21 -0400

    finish basic thread component

--- client/src/components/Thread.js
+++ client/src/components/Thread.js
@@ -1,28 +1,34 @@
 import React from 'react'
+import './Thread.css'
 
 class Thread extends React.Component {
     render() {
         return (
             <div className='single-thread-page'>
-                <div className='thread page title'>
+                <div className='thread page title'> </div>
+
+                <div className='threadtitle'>
                     <h1>Thread Title One</h1>
                 </div>
+           <div className='wrap-thread-boxes'>
                 <div className='threadbox'>
-                    <h2>Thread title. By'name' created on'date'</h2>
+                    <h2 class>Thread title. By'name' created on'date'</h2>
                     <p>Loremn jbrebh wahrbe jrhgjk grwbjg wrgb</p>
-
+                  </div>
 
                     <div className='threadpost'>
                       <h3>Respond to this post</h3>
+                      <form id="thread-form">
                         <input type='text' placeholder='Add subject here'></input>
                         <input type='text'className='commentbox' placeholder='Enter comments here'></input>
                         <button>Post</button>
 
-                    </div>
+                        </form>
+              
+                    
                 </div>
 
-
-
+</div>
 
             </div>
         )
