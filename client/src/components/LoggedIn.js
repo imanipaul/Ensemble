53bcf7650824ae2be567333604e1582f51189942
imanipaul <imanipaul@wustl.edu>
2019-04-11 14:03:45 -0400

    Add form for thread post

--- client/src/components/LoggedIn.js
+++ client/src/components/LoggedIn.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 class LoggedIn extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
     render() {
         return (
-            <div>This is the Logged In page</div>
+            <div className='home-page'>
+                <header>
+                    <h1>Welcome</h1>
+                    <form name='thread-post'>
+                        <input type='text' placeholder='Add title here'></input>
+
+                    </form>
+                    <textarea rows='4' cols='40' name='post' form='thread-post'>Enter post here...</textarea>
+                </header>
+
+            </div>
         )
     }
 }
