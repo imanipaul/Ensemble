be67bc6b80cbdc66e9d53bc147f38d3e2a7c9fbf
imanipaul <imanipaul@wustl.edu>
2019-04-11 12:33:34 -0400

    Create login form

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -9,7 +9,31 @@ class LandingPage extends React.Component {
 
     render() {
         return (
-            <div>This is the landing page</div>
+            <div className='landing-page'>
+                <nav>
+                    <h1>Logo</h1>
+                    <a href='#'>Cat1</a>
+                    <a href='#'>Cat2</a>
+                    <a href='#'>Cat3</a>
+                </nav>
+                <div>
+                    <h2>Recent Posts</h2>
+                    <a href="#">Thread 1</a>
+                    <a href="#">Thread 2</a>
+                    <a href="#">Thread 3</a>
+                    <a href="#">Thread 4</a>
+                    <a href="#">Thread 5</a>
+                </div>
+                <h2>LogIn/SignUp</h2>
+                <form>
+
+                    <input type='text' placeholder='name'></input>
+                    <input type='text' placeholder='email'></input>
+                    <button>Login</button>
+                    <button>Signup</button>
+                </form>
+
+            </div>
         )
     }
 }
