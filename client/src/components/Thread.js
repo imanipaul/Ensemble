ecc941cdae6a80bd4e1d8c177f57aae70a2fda15
BIB007$ <bib7@ool-ad020186.dyn.optonline.net>
2019-04-11 15:33:24 -0400

    create thread page

--- client/src/components/Thread.js
+++ client/src/components/Thread.js
@@ -3,7 +3,28 @@ import React from 'react'
 class Thread extends React.Component {
     render() {
         return (
-            <div>This is the thread page</div>
+            <div className='single-thread-page'>
+                <div className='thread page title'>
+                    <h1>Thread Title One</h1>
+                </div>
+                <div className='threadbox'>
+                    <h2>Thread title. By'name' created on'date'</h2>
+                    <p>Loremn jbrebh wahrbe jrhgjk grwbjg wrgb</p>
+
+
+                    <div className='threadpost'>
+                      <h3>Respond to this post</h3>
+                        <input type='text' placeholder='Add subject here'></input>
+                        <input type='text'className='commentbox' placeholder='Enter comments here'></input>
+                        <button>Post</button>
+
+                    </div>
+                </div>
+
+
+
+
+            </div>
         )
     }
 }
