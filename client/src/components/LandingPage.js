37d529e1eff0530354f1693d1b216a6c366062fb
imanipaul <imanipaul@wustl.edu>
2019-04-11 12:27:48 -0400

    Create individual CSS files

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class LandingPage extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+
     render() {
         return (
             <div>This is the landing page</div>
