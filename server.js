b458c532f46e304228236bbb1863bef797870b1c
icelydone <soren03@hotmail.com>
2019-04-12 15:45:05 -0400

    Completed basic auth

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 
+const { Op } = require('sequelize')
+
+const { User } = require('./models/models')
+
+const { hashPassword, checkPassword, genToken } = require('./auth.js')
+
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -12,12 +18,62 @@ app.get('/', (req, res) => {
   res.send('This is the home page of the server')
 })
 
-app.get('/login', (req, res) => {
-    
+app.post('/login', async (req, res) => {
+  let user
+  const { nameOrEmail, password } = req.body
+  if (nameOrEmail.search(/@/) !== -1) {
+    user = await User.findOne({
+      where: {
+        email: {
+          [Op.iLike]: nameOrEmail
+        }
+      }
+    })
+  } else {
+    user = await User.findOne({
+      where: {
+        name: {
+          [Op.iLike]: nameOrEmail
+        }
+      }
+    })
+  }
+  if (user) {
+    const isUser = await checkPassword(password, user.password_digest)
+    if (isUser) {
+      const token = genToken(user)
+      res.json({
+        token,
+        user,
+        message: 'Logged in!'
+      })
+    } else {
+      res.json({
+        message: 'Incorrect password'
+      })
+    }
+  } else {
+    res.json({
+      message: 'Incorrect name or email'
+    })
+  }
 })
 
-app.get('/signup', (req, res) => {
-    
+app.post('/signup', async (req, res) => {
+  const { email, password, name } = req.body
+  const passwordDigest = await hashPassword(password)
+  const user = User.create({
+    name,
+    email,
+    password_digest: passwordDigest
+  })
+  console.log(user)
+  const token = genToken(user)
+  res.json({
+    token,
+    user,
+    message: 'User successfully created'
+  })
 })
 
 app.listen(Port, () => console.log(`Running on port ${Port}`))
