3b0a5a128382d7e6b5139e781ce76f2e58861c0d
imanipaul <imanipaul@wustl.edu>
2019-04-12 08:46:53 -0400

    Create GET users endpoint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { User, Category, Thread, Comment } = require('./models/models')
+const { User, Category, Thread, Comment } = require('./models/models.js')
 const { Op } = require('sequelize')
 
 const app = express()
@@ -13,6 +13,18 @@ const Port = process.env.PORT || 1340
 
 app.get('/', (req, res) => {
   res.send('This is the home page of the server')
+})
+
+
+app.get('/users', async (req, res) => {
+  try {
+    const allUsers = await User.findAll({ raw: true })
+    res.json({ allUsers })
+    console.log(JSON.stringify(allUsers, null, 2))
+  }
+  catch (e) {
+    console.log(e.message)
+  }
 })
 
 app.listen(Port, () => console.log(`Running on port ${Port}`))
