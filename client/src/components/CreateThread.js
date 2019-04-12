4389313a1844b6898630e63d977c63071534784d
imanipaul <imanipaul@wustl.edu>
2019-04-12 09:54:48 -0400

    Create onFormChange and onFormSubmit functions

---
+++ client/src/components/CreateThread.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const url = 'http://localhost:1340/'
+
+class CreateThread extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: '',
+            content: ''
+        }
+    }
+
+    onFormChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefaul()
+        let data = {
+            title: this.state.title,
+            content: this.state.content
+        };
+
+        fetch(`${url}/thread`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            return response.json()
+        })
+
+        this.setState({
+            title: '',
+            content: '',
+        })
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+
+
+
+
+
+}
