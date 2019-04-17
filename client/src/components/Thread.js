ec1007af10958a2d148451dccd417fca87af178a
Imani Paul <31520307+imanipaul@users.noreply.github.com>
2019-04-16 20:40:08 -0400

    Revert "Category update fix"

--- client/src/components/Thread.js
+++ client/src/components/Thread.js
@@ -33,15 +33,16 @@ class Thread extends React.Component {
     }
 
     getComments = async () => {
+        // console.log('comments prop:', this.state.comments)
         let currentId = parseInt(this.state.threadId)
-        const response = await fetch(url + '/comment')
-        const data = await response.json()
-        const threadComments = await data.filter(comment => {
+        const threadComments = await this.props.comments.filter(comment => {
             return comment.threadId === currentId
         })
+        await console.log('threadComments:', threadComments)
         this.setState({
             threadComments: threadComments
         })
+        // this.props.refreshComments()
     }
 
     componentDidMount() {
@@ -52,7 +53,9 @@ class Thread extends React.Component {
     render() {
         const createTime = new Date(this.state.thread.createdAt)
         let currentId = parseInt(this.state.threadId)
-        console.log(this.state.threadComments)
+        const threadComments = this.props.comments.filter(comment => {
+            return comment.threadId === currentId
+        })
         let threadUser = this.props.users.find(user => user.id === this.state.thread.userId)
         if (!threadUser) threadUser = { name: '' }
         console.log(threadUser)
@@ -69,21 +72,21 @@ class Thread extends React.Component {
                         <p className='thread_created_date'>Created on {createTime.toLocaleString()}</p>
                         <p className='thread_content'>{this.state.thread.content}</p>
 
-                       {this.props.currentUser.id === this.state.thread.userId && 
-                          <button className=" delete_button" id={this.state.threadId} onClick={event => {
-                              this.props.handleDeleteThreads(event);
-                              this.props.history.push('/category/' + this.state.thread.categoryId)
-                          }} >Delete</button>
-                       }
-  
-                       {this.props.currentUser.id === this.state.thread.userId && 
-                          <button className="update_button" onClick={(event) => {
-                              event.preventDefault();
-                              this.setState({
-                                  update: !this.state.update
-                              })
-                          }}>Edit</button>
-                       }
+                        {this.props.currentUser.id === this.state.thread.userId &&
+                            <button className=" delete_button" id={this.state.threadId} onClick={event => {
+                                this.props.handleDeleteThreads(event);
+                                this.props.history.push('/')
+                            }} >Delete</button>
+                        }
+
+                        {this.props.currentUser.id === this.state.thread.userId &&
+                            <button className="update_button" onClick={(event) => {
+                                event.preventDefault();
+                                this.setState({
+                                    update: !this.state.update
+                                })
+                            }}>Edit</button>
+                        }
 
                         <div>
 
@@ -111,17 +114,16 @@ class Thread extends React.Component {
 
                     <UpdateThread threadId={this.state.threadId} update={this.state.update} getComments={this.getComments} />
 
-                    {this.props.currentUser.id &&
                     <div className='comment_widget'>
-                        <CreateComment getComments={this.props.getComments} threadId={this.state.threadId} update={this.update} getComments={this.getComments} />
-
-                   </div>
-                    }
+                        {this.props.currentUser.id &&
+                            <CreateComment getComments={this.props.getComments} threadId={this.state.threadId} update={this.update} getComments={this.props.refreshComments} />
+                        }
+                    </div>
 
                     {/* <div>
 
-                        {!!this.state.threadComments.length &&
-                            this.state.threadComments.map(comment => (
+                        {!!threadComments.length &&
+                            threadComments.map(comment => (
                                 <div className="thread_comment_container">
                                     <p key={comment.id}>{comment.content}</p>
                                 </div>
